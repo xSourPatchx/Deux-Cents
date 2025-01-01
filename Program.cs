32faad08f0c248b2411cc6987326a3e3f0134554
Pat <60205594+xSourPatchx@users.noreply.github.com>
2024-12-31 22:55:25 -0400

    Update Program.cs
    cleaning up code

--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
-﻿using System;
+using System;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
 
 namespace CardGame
 {
-    class Program 
+    class Program
     {
         static void Main(string[] args)
         {
+            // Will need below in unity, to check if card slot has a card or is empty once card are played
+            //bool[] playerOneAvailableCardSlot = { true, true, true, true, true, true, true, true, true, true };
+            //bool[] playerTwoAvailableCardSlot = { true, true, true, true, true, true, true, true, true, true };
+            //bool[] playerThreeAvailableCardSlot = { true, true, true, true, true, true, true, true, true, true };
+            //bool[] playerFourAvailableCardSlot = { true, true, true, true, true, true, true, true, true, true };
+
+
+            /* SECTION 1: INITIAL CARDS, SHUFFLE AND DEAL */
+
+            Console.WriteLine("Shuffling and dealing cards...");
+            Thread.Sleep(2000);
+
             // init list of objects
             List<Card> deck = new List<Card>();
 
-            // append each card
-            // clubs
-            deck.Add(new Card("5", "clubs", 1, 5));
+            // append each card to deck
+            deck.Add(new Card("5", "clubs", 1, 5)); // clubs
             deck.Add(new Card("6", "clubs", 2, 0));
             deck.Add(new Card("7", "clubs", 3, 0));
             deck.Add(new Card("8", "clubs", 4, 0));
@@ -22,9 +35,7 @@ namespace CardGame
             deck.Add(new Card("Q", "clubs", 8, 0));
             deck.Add(new Card("K", "clubs", 9, 0));
             deck.Add(new Card("A", "clubs", 10, 10));
-
-            // diamonds
-            deck.Add(new Card("5", "diamonds", 1, 5));
+            deck.Add(new Card("5", "diamonds", 1, 5)); // diamonds
             deck.Add(new Card("6", "diamonds", 2, 0));
             deck.Add(new Card("7", "diamonds", 3, 0));
             deck.Add(new Card("8", "diamonds", 4, 0));
@@ -34,9 +45,7 @@ namespace CardGame
             deck.Add(new Card("Q", "diamonds", 8, 0));
             deck.Add(new Card("K", "diamonds", 9, 0));
             deck.Add(new Card("A", "diamonds", 10, 10));
-
-            // hearts
-            deck.Add(new Card("5", "hearts", 1, 5));
+            deck.Add(new Card("5", "hearts", 1, 5)); // hearts
             deck.Add(new Card("6", "hearts", 2, 0));
             deck.Add(new Card("7", "hearts", 3, 0));
             deck.Add(new Card("8", "hearts", 4, 0));
@@ -46,9 +55,7 @@ namespace CardGame
             deck.Add(new Card("Q", "hearts", 8, 0));
             deck.Add(new Card("K", "hearts", 9, 0));
             deck.Add(new Card("A", "hearts", 10, 10));
-
-            // spades
-            deck.Add(new Card("5", "spades", 1, 5));
+            deck.Add(new Card("5", "spades", 1, 5)); // spades
             deck.Add(new Card("6", "spades", 2, 0));
             deck.Add(new Card("7", "spades", 3, 0));
             deck.Add(new Card("8", "spades", 4, 0));
@@ -59,27 +66,16 @@ namespace CardGame
             deck.Add(new Card("K", "spades", 9, 0));
             deck.Add(new Card("A", "spades", 10, 10));
 
-
-            // disp deck of cards
-            foreach (Card card in deck) { Console.WriteLine(card); }
-
-            Console.WriteLine("\n#########################\n");
-
+            // shuffle deck 3 times
             ShuffleDeck();
             ShuffleDeck();
             ShuffleDeck();
 
-            // disp deck of cards
-            foreach (Card card in deck) { Console.WriteLine(card); }
-
-            Console.WriteLine("\n#########################\n");
-
-
+            // create 4 list for all 4 players 
             List<Card> playerOne = new List<Card>();
             List<Card> playerTwo = new List<Card>();
             List<Card> playerThree = new List<Card>();
             List<Card> playerFour = new List<Card>();
-
 
             for (int i = 0; i < (deck.Count); i += 4)
             {
@@ -91,44 +87,32 @@ namespace CardGame
 
             Console.WriteLine("\n#########################\n");
 
-            // disp playerOne hand
-            foreach (Card player in playerOne){ Console.WriteLine($"Player One Hand:\n {player}"); }
+            Console.WriteLine("Player one's hand:\n");
+            foreach (Card player in playerOne) { Console.WriteLine(player); } // disp playerOne hand
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("\nPlayer two's hand:\n");
+            foreach (Card player in playerTwo) { Console.WriteLine(player); } // disp playerTwo hand
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("\nPlayer three's hand:\n");
+            foreach (Card player in playerThree) { Console.WriteLine(player); } // disp playerThree hand
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("\nPlayer four's hand:\n");
+            foreach (Card player in playerFour) { Console.WriteLine(player); } // disp playerFour hand
 
             Console.WriteLine("\n#########################\n");
 
-            // disp playerOne hand
-            foreach (Card player in playerTwo) { Console.WriteLine($"Player Two Hand:\n {player}"); }
 
-            Console.WriteLine("\n#########################\n");
+            /* SECTION 2: BETTING ROUND */ // should make this a method
 
-            // disp playerOne hand
-            foreach (Card player in playerThree) { Console.WriteLine($"Player Three Hand:\n {player}"); }
+            Console.WriteLine("Betting round\n");
+            Thread.Sleep(1000);
 
-            Console.WriteLine("\n#########################\n");
-
-            // disp playerOne hand
-            foreach (Card player in playerFour) { Console.WriteLine($"Player Four Hand:\n {player}"); }
-
-
-            Console.WriteLine("\n#########################\n");
-
-            List<Card> trick = new List<Card>();
-            for (int i = 0; i < 4; i++)
-            {
-                trick.Add(deck[i]);
-            }
-
-            foreach (Card card in trick) { Console.WriteLine(card); }
-
-            Console.WriteLine("\n#########################\n");
-
-            Card cardwinner = TrickWinner(trick[0], trick[1], trick[2], trick[3]); // test
-            Console.WriteLine(cardwinner); // test
-
-            Console.WriteLine("\n#########################\n");
-
-            
-            /* BETTING ROUND SECTION */
             List<string> players = new List<string> { "Player One", "Player Two", "Player Three", "Player Four" };
             List<int> bets = new List<int>(); // store bets
             int passCount = 0;
@@ -141,12 +125,12 @@ namespace CardGame
                     if (bets.Count > i && bets[i] == -1)
                     { continue; }
 
-                    Console.WriteLine($"\n{ players[i] }, enter a bet (between 50-100, intervals of 5) or 'pass': ");
+                    Console.WriteLine($"{players[i]}, enter a bet (between 50-100, intervals of 5) or 'pass': ");
                     string input = Console.ReadLine().ToLower();
 
                     if (input == "pass")
                     {
-                        Console.WriteLine($"{players[i]} passed");
+                        Console.WriteLine($"{players[i]} passed\n");
                         if (bets.Count <= i)
                             bets.Add(-1);
                         else
@@ -156,11 +140,12 @@ namespace CardGame
                     else if (int.TryParse(input, out int bet))
                     {
                         if (bet >= 50 && bet <= 100 && bet % 5 == 0 && !bets.Contains(bet))
-                        {   
+                        {
                             if (bets.Count <= i)
                                 bets.Add(bet);
                             else
                                 bets[i] = bet;
+                            Console.WriteLine();
                         }
                         else
                         {
@@ -177,14 +162,13 @@ namespace CardGame
                     if (passCount >= 3)
                     {
                         Console.WriteLine("Betting round ends");
-                        
                         // inserting default bet of 50 to player 4 since all prior players passed.
-                        if (bets.Count == 4) 
+                        if (bets.Count == 4)
                         {
                             bettingRoundEnded = true;
                             break;
                         }
-                        else 
+                        else
                         {
                             bets.Add(50);
                             bettingRoundEnded = true;
@@ -203,8 +187,25 @@ namespace CardGame
 
 
 
-            
-            // Shuffle deck method
+            //// test below
+            //List<Card> testTrick = new List<Card>();
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    testTrick.Add(deck[i]);
+            //}
+
+            //foreach (Card card in testTrick) { Console.WriteLine(card); }
+
+            //Console.WriteLine("\n#########################\n");
+
+            //// call method to find winner of trick
+            //Card cardwinner = TrickWinner(testTrick[0], testTrick[1], testTrick[2], testTrick[3]);
+            //Console.WriteLine(cardwinner);
+
+            //Console.WriteLine("\n#########################\n");
+
+
+            // method to shuffle deck
             void ShuffleDeck()
             {
                 int n = 0;
@@ -219,8 +220,8 @@ namespace CardGame
                 }
             }
 
-            // Trick winner method
-            Card TrickWinner(Card playerOneCard, Card playerTwoCard, Card playerThreeCard, Card playerFourCard) 
+            // method to select winner of trick (not yet used)
+            Card TrickWinner(Card playerOneCard, Card playerTwoCard, Card playerThreeCard, Card playerFourCard)
             {
                 List<Card> trick = new List<Card>();
                 trick.Add(playerOneCard);
@@ -230,13 +231,12 @@ namespace CardGame
                 int maxVal = trick.Max(x => x.cardFaceValue);
                 Card maxCard = trick.First(x => x.cardFaceValue == maxVal);
                 return maxCard;
+            }
 
-                
             Console.ReadKey();
         }
     }
 
-    // class of cards
     class Card
     {
         public string cardFace; // 5, 6, 7, 8, 9, 10, J, Q, K, A
@@ -253,7 +253,7 @@ namespace CardGame
 
         public override string ToString()
         {
-            return cardFace + "("+cardFaceValue + ")" + " of " + cardSuit + " = " + cardPointValue + " points";
+            return cardFace + "(" + cardFaceValue + ")" + " of " + cardSuit + " = " + cardPointValue + " points";
         }
     }
 }
